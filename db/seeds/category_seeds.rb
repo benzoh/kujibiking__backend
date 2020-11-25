732482265833d1698436e279ed3082c1390b4500
Mitsuhiro Wada <benzoh.wada@gmail.com>
2020-11-25 13:08:48 +0000

    add seed

--- db/seeds/category_seeds.rb
+++ db/seeds/category_seeds.rb
@@ -3,7 +3,7 @@
 unless Category.exists?
   count = 1
   20.times do
-    Category.create!(name: "test" + count , slug: "test_" + count)
+    Category.create!(name: "test" + count.to_s , slug: "test_" + count.to_s)
     count += 1
   end
 end
