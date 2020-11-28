b21fa5549fe5828cddba2d1b5389b565249ee886
Benzoh <benzoh.wada@gmail.com>
2020-11-28 12:19:27 +0000

    devise_token_auth導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # skip_before_action :verify_authenticity_token
-  # skip_before_action :verify_authenticity_token, if: :devise_controller? # APIではCSRFチェックをしない
-
-  # raise
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def render_404
     render status: 404, json: { message: "record not found." }
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
   namespace :api do
-    # localhost:3000/api/v1/auth に認証API
-    mount_devise_token_auth_for 'User', at: '/v1/auth'
-
-    # mount API::Root => '/'
-
     namespace 'v1' do
       resources :lotteries
       resources :categories
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
