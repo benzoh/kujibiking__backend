285cdc508945323a7f130351395f331f77181cd3
Benzoh <benzoh.wada@gmail.com>
2020-11-29 13:10:50 +0000

    user model test

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    provider { "email" }
+    sequence(:email) { |n| "test#{n}@example.com" }
+    uid { email }
+    password { "password" }
+    remember_created_at { nil }
+    sequence(:name) { |n| "user#{n}" }
+    tokens { nil }
+  end
+end
