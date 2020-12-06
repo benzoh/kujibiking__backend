b5b896a843db5872c29bf6fb2c87b116dc88b508
Benzoh <benzoh.wada@gmail.com>
2020-12-06 01:45:59 +0000

    WIP: user model seliarize

---
+++ app/controllers/api/v1/overrides/sessions_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module V1
+    module Overrides
+      class SessionsController < ::DeviseTokenAuth::SessionsController
+
+        # override this method to customise how the resource is rendered. in this case an ActiveModelSerializers 0.10 serializer.
+        def render_create_success
+          render json: { data: ActiveModelSerializers::SerializableResource.new(@resource).as_json }
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     namespace 'v1' do
       resources :lotteries
       resources :categories
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        sessions: 'api/v1/overrides/sessions'
+      }
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
