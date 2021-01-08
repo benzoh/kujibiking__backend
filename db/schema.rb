6b13254800adc1cd5496585e353eb907d6c7312d
Benzoh <benzoh.wada@gmail.com>
2021-01-08 20:52:19 +0000

    userモデルにadminカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_134359) do
+ActiveRecord::Schema.define(version: 2021_01_08_204722) do
 
   create_table "applies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "product_id"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_134359) do
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
