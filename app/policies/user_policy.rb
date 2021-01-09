4e93663c58c5a777e04d83ad6adbd95130599cab
Benzoh <benzoh.wada@gmail.com>
2021-01-09 21:09:25 +0000

    feat: user_policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,12 +1,34 @@
 # frozen_string_literal: true
 
-#
 # user policy class
-#
 class UserPolicy < ApplicationPolicy
-  #
+  def index?
+    admin?
+  end
+
+  def show?
+    me? || admin?
+  end
+
+  def create?
+    admin?
+  end
+
+  def update?
+    me? || admin?
+  end
+
+  def destroy?
+    admin?
+  end
+
+  private
+
+  def me?
+    @record == @user
+  end
+
   # scope
-  #
   class Scope < Scope
     def resolve
       scope.all
