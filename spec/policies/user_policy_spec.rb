c96232f9ee813c081ffad803d98ec4a952c4d221
Benzoh <benzoh.wada@gmail.com>
2021-01-09 20:50:50 +0000

    add user_policy test

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe UserPolicy, type: :policy do
   let(:user) { User.new }
+  let(:another_user) { create(:user) }
+  let(:admin_user) { create(:user, :admin) }
 
   subject { described_class }
 
@@ -11,19 +13,30 @@ RSpec.describe UserPolicy, type: :policy do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :index?, :create?, :destroy? do
+    it '未ログインの時に不許可' do
+      expect(subject).not_to permit(nil, user)
+    end
+    it '管理者でなければ不可' do
+      expect(subject).not_to permit(user, user)
+    end
+    it '管理者のみ閲覧可能' do
+      expect(subject).to permit(admin_user, user)
+    end
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :show?, :update? do
+    it '未ログインの時に不許可' do
+      expect(subject).not_to permit(nil, user)
+    end
+    it 'ログインしているが別ユーザーの時に不許可' do
+      expect(subject).not_to permit(user, another_user)
+    end
+    it '管理者は閲覧可能' do
+      expect(subject).to permit(admin_user, user)
+    end
+    it '自分自身は閲覧可能' do
+      expect(subject).to permit(user, user)
+    end
   end
 end
